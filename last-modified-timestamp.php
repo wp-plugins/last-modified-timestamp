d8473256d6cf78bd3ce5f8eb09eade66857bc498
aaemnnosttv <aaemnnosttv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-11 02:37:57 +0000

    0.2 already, whoorraaaa!!
    git-svn-id: https://plugins.svn.wordpress.org/last-modified-timestamp/tags/0.2@487908 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- last-modified-timestamp.php
+++ last-modified-timestamp.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Last Modified Timestamp
 Description: This plugin will add information to the admin interface about when each post/page was last modified. No options currently available, simply activate and enjoy!
-Version: trunk
+Version: 0.2
 Author: Evan Mattson
 */
 
@@ -23,70 +23,71 @@ Author: Evan Mattson
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-
-$the_date_format = 'M j, Y'; 
-$user_time_format = get_option('time_format'); // use user's preferred time formatting
-
-// redefines the admin messages at the top of the page on post.php for pages & posts.
-function modify_messages($messages) {
-global $post, $the_date_format, $user_time_format;
-
-$modified_timestamp = date_i18n( __( $the_date_format . ' @ ' . $user_time_format ), strtotime( $post->post_modified ) );
-
-
-$messages['post'] = array(
-	 1 => sprintf( __('Post updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
-	 4 => sprintf( __('Post updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
-	);
-$messages['page'] = array(
-	 1 => sprintf( __('Page updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
-	 4 => sprintf( __('Page updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
-	);
-return $messages;
-}
-
-// now let's add the last modified timestamp to the post meta box in post.php!
-function add_modified_to_meta() {
-	global $post, $the_date_format, $user_time_format;
-
-	$modified_timestamp = sprintf( __('Last modified on: <strong>%1$s</strong>'), date_i18n( __( $the_date_format . ' @ ' . $user_time_format ), strtotime( $post->post_modified ) ) );
-	  
-	echo '<div class="misc-pub-section misc-pub-section-last">' . $modified_timestamp . '</div>';
-}
-
-// Now for the post/pages table column
-
-// Append the new column to the columns array
-function last_modified_column_heading($columns) {
-  $columns['last-modified'] = 'Last Modified';
-  return $columns;
-}
-// Put the last modified date in the content area
-function last_modified_column_content($column_name, $id) {
-global $post, $user_time_format;
-	if ($column_name == 'last-modified')
-	echo ( date_i18n( __( 'Y/n/j\<\b\r\ \/\>' . $user_time_format ), strtotime( $post->post_modified ) ) ) ;
-}
-
-// Output CSS for width of new column
-function last_modified_css() {
-?>
-<style type="text/css">
-  #last-modified { width: 110px; }
-</style>
-<?php  
-}
-
 function last_modified_add() {
-	add_action('admin_head', 'last_modified_css');
+	add_action( 'admin_head', 'print_last_modified_css' );
 	
 	add_filter( 'post_updated_messages','modify_messages' );
 	add_action( 'post_submitbox_misc_actions', 'add_modified_to_meta' );
 
-	add_filter('manage_page_posts_columns','last_modified_column_heading');
-	add_filter('manage_post_posts_columns','last_modified_column_heading');
-	add_action('manage_pages_custom_column','last_modified_column_content', 10, 2);
-	add_action('manage_posts_custom_column','last_modified_column_content', 10, 2);
+	add_filter( 'manage_page_posts_columns','last_modified_column_heading' );
+	add_filter( 'manage_post_posts_columns','last_modified_column_heading' );
+	add_action( 'manage_pages_custom_column','last_modified_column_content', 10, 2 );
+	add_action( 'manage_posts_custom_column','last_modified_column_content', 10, 2 );
 }
-add_action('admin_init', 'last_modified_add');
+add_action( 'admin_init', 'last_modified_add' );
+
+// returns a formatted timestamp as a string
+function construct_timestamp($mode='') { 
+// instead of IF to better handle other possible modes in future
+	switch ($mode) { 
+		case 'wp-table' :
+			$modified_timestamp = get_the_modified_date('Y/m/d') . '<br />' . get_the_modified_time();  // wp-tables
+			break;
+		default :
+			$modified_timestamp = get_the_modified_date('M j, Y') . ' @ ' . get_the_modified_time(); //  post messages & meta box
+	}
+return $modified_timestamp;
+}
+
+// filters the admin messages at the top of the page on post.php for pages & posts to include the last modified timestamp.
+function modify_messages($messages) {
+	$modified_timestamp = construct_timestamp();
+	$messages['post'] = array(
+		 1 => sprintf( __('Post updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
+		 4 => sprintf( __('Post updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
+		);
+	$messages['page'] = array(
+		 1 => sprintf( __('Page updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
+		 4 => sprintf( __('Page updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
+		);
+	return $messages;
+}
+
+// Add the Last Modified timestamp to the 'Publish' meta box in post.php
+function add_modified_to_meta() {
+	$modified_timestamp = sprintf( __('Last modified on: <strong>%1$s</strong>'), construct_timestamp() );
+	echo '<div class="misc-pub-section misc-pub-section-last">' . $modified_timestamp . '</div>';
+}
+
+// The next 2 functions add the column in admin post/page tables :
+
+// Append the new column to the columns array
+function last_modified_column_heading($columns) {
+	$columns['last-modified'] = 'Last Modified';
+	return $columns;
+}
+// Put the last modified date in the content area
+function last_modified_column_content($column_name, $id) {
+	if ($column_name == 'last-modified')
+		echo construct_timestamp('wp-table');
+}
+
+// Output CSS for width of new column 
+function print_last_modified_css() {
+?>
+<style type="text/css">
+	#last-modified { width: 110px; }
+</style>
+<?php  
+}
 ?>
