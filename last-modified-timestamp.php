fbe375292c843a1d624358dfbede4fb54a611ec2
aaemnnosttv <aaemnnosttv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-11 01:46:58 +0000

    initial trunk
    git-svn-id: https://plugins.svn.wordpress.org/last-modified-timestamp/trunk@487873 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ last-modified-timestamp.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Plugin Name: Last Modified Timestamp
+Description: This plugin will add information to the admin interface about when each post/page was last modified. No options currently available, simply activate and enjoy!
+Version: trunk
+Author: Evan Mattson
+*/
+
+/*  Copyright 2011 Evan Mattson (email: evanmattson at gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+$the_date_format = 'M j, Y'; 
+$user_time_format = get_option('time_format'); // use user's preferred time formatting
+
+// redefines the admin messages at the top of the page on post.php for pages & posts.
+function modify_messages($messages) {
+global $post, $the_date_format, $user_time_format;
+
+$modified_timestamp = date_i18n( __( $the_date_format . ' @ ' . $user_time_format ), strtotime( $post->post_modified ) );
+
+
+$messages['post'] = array(
+	 1 => sprintf( __('Post updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
+	 4 => sprintf( __('Post updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
+	);
+$messages['page'] = array(
+	 1 => sprintf( __('Page updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
+	 4 => sprintf( __('Page updated. <strong>%2$s</strong>. <a href="%1$s">View post</a>'), esc_url( get_permalink($post_ID) ), $modified_timestamp ),
+	);
+return $messages;
+}
+
+// now let's add the last modified timestamp to the post meta box in post.php!
+function add_modified_to_meta() {
+	global $post, $the_date_format, $user_time_format;
+
+	$modified_timestamp = sprintf( __('Last modified on: <strong>%1$s</strong>'), date_i18n( __( $the_date_format . ' @ ' . $user_time_format ), strtotime( $post->post_modified ) ) );
+	  
+	echo '<div class="misc-pub-section misc-pub-section-last">' . $modified_timestamp . '</div>';
+}
+
+// Now for the post/pages table column
+
+// Append the new column to the columns array
+function last_modified_column_heading($columns) {
+  $columns['last-modified'] = 'Last Modified';
+  return $columns;
+}
+// Put the last modified date in the content area
+function last_modified_column_content($column_name, $id) {
+global $post, $user_time_format;
+	if ($column_name == 'last-modified')
+	echo ( date_i18n( __( 'Y/n/j\<\b\r\ \/\>' . $user_time_format ), strtotime( $post->post_modified ) ) ) ;
+}
+
+// Output CSS for width of new column
+function last_modified_css() {
+?>
+<style type="text/css">
+  #last-modified { width: 110px; }
+</style>
+<?php  
+}
+
+function last_modified_add() {
+	add_action('admin_head', 'last_modified_css');
+	
+	add_filter( 'post_updated_messages','modify_messages' );
+	add_action( 'post_submitbox_misc_actions', 'add_modified_to_meta' );
+
+	add_filter('manage_page_posts_columns','last_modified_column_heading');
+	add_filter('manage_post_posts_columns','last_modified_column_heading');
+	add_action('manage_pages_custom_column','last_modified_column_content', 10, 2);
+	add_action('manage_posts_custom_column','last_modified_column_content', 10, 2);
+}
+add_action('admin_init', 'last_modified_add');
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Last Modified Timestamp ===
+Contributors: aaemnnosttv
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LRA4JZYALHX82
+Tags: page modified, post modified, timestamp, last modified
+Requires at least: 3.0.0
+Tested up to: 3.1.1
+Stable tag: 0.1
+
+Adds the post_modified timestamp to the admin interface. No options currently available, simply activate and enjoy!
+
+== Description ==
+
+This plugin adds the information to the admin interface about when each post/page was last modified. 
+
+Enhanced areas:
+
+1. Page/post admin tables - added `Last Modified` column.
+1. Page/post edit screen (`post.php`) - added `Last modified on: *timestamp*` to `Publish` meta box.
+1. Admin messages after editing a page/post - ie: `Post updated. *timestamp* View Post`
+
+No options currently available, simply activate and enjoy!
+
+== Installation ==
+
+1. Upload the `last-modified-timestamp` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+
+== Frequently Asked Questions ==
+
+*none yet*
+
+Email your questions to `evanmattson` *at* `gmail` *dot* `com`
+
+
+== Screenshots ==
+
+1. Page/post admin tables - added `Last Modified` column.
+1. Page/post edit screen (`post.php`) - added `Last modified on: *timestamp*` to `Publish` meta box.
+1. Admin messages after editing a page/post - ie: `Post updated. *timestamp* View Post`
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
